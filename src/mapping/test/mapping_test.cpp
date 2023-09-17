822c8317f87a7bbd53673cafd22bad0cd3669023
Arthur Xu <arthurxu011@gmail.com>
2023-09-16 21:58:19 -0400

    Added unit test file

---
+++ src/mapping/test/mapping_test.cpp
@@ -0,0 +1,30 @@
+/*
+██████  ██    ██ ██████  ██   ██
+██   ██ ██    ██      ██ ██   ██
+██   ██ ██    ██  █████  ███████
+██   ██  ██  ██  ██           ██
+██████    ████   ███████      ██
+*
+* file: mapping_test.cpp
+* auth: Arthur Xu
+* desc: mapping unit tests
+*/
+
+
+#include <gtest/gtest.h>
+#include <math.h>
+#include <rclcpp/rclcpp.hpp>
+#include <build_graph_node.hpp>
+#include <compute_graph_node.hpp>
+#include <ekf_node.hpp>
+
+using namespace utfr_dv::build_graph;
+using namespace utfr_dv::compute_graph;
+using namespace utfr_dv::ekf;
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
