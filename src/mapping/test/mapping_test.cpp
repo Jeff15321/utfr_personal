eb92c45f2c11522696285576ec26316f984f5eac
Arthur Xu <arthurxu011@gmail.com>
2023-09-19 11:48:24 -0400

    more changes

--- src/mapping/test/mapping_test.cpp
+++ src/mapping/test/mapping_test.cpp
@@ -38,31 +38,40 @@ TEST(BuildGraphNodeTest, kNNTest1)
     // First round of cone detections
     utfr_msgs::msg::ConeDetections cones;
 
-    utfr_msgs::msg::ConeDetection blue_cone;
-    cone.pose.pose.position.x = 1;
-    cone.pose.pose.position.y = 1;
-    cone.pose.pose.position.z = 0;
-    cone.pose.pose.type = 1;
+    utfr_msgs::msg::Cone blue_cone;
+    blue_cone.pos.x = 1;
+    blue_cone.pos.y = 1;
+    blue_cone.pos.z = 0;
+    blue_cone.type = 1;
 
-    utfr_msgs::msg::ConeDetection yellow_cone;
-    cone.pose.pose.position.x = -1;
-    cone.pose.pose.position.y = -1;
-    cone.pose.pose.position.z = 0;
-    cone.pose.pose.type = 2;
+    utfr_msgs::msg::Cone yellow_cone;
+    yellow_cone.pos.x = -1;
+    yellow_cone.pos.y = -1;
+    yellow_cone.pos.z = 0;
+    yellow_cone.type = 2;
 
     cones.left_cones.push_back(blue_cone);
     cones.right_cones.push_back(yellow_cone);
     node.KNN(cones);
-    ASSERT_EQ(1, node.current_cone_map_.left_cones.size());
-    ASSERT_EQ(1, node.current_cone_map_.right_cones.size());
+    ASSERT_EQ(2, node.past_detections_.size());
 
     // We detect the same cones again, but nothing should happen, as we
     // already know of their existences.
 
     node.KNN(cones);
-    ASSERT_EQ(1, node.current_cone_map_.left_cones.size());
-    ASSERT_EQ(1, node.current_cone_map_.right_cones.size());
+    ASSERT_EQ(2, node.past_detections_.size());
 
+    // We now find a new cone, and it should be added to the hashmap.
+    utfr_msgs::msg::Cone yellow_cone2;
+    yellow_cone2.pos.x = -1;
+    yellow_cone2.pos.y = 1;
+    yellow_cone2.pos.z = 0;
+    yellow_cone2.type = 2;
+    cones.right_cones.push_back(yellow_cone2);
+
+    // It is a new cone, thus it should be added to the hashmap.
+    node.KNN(cones);
+    ASSERT_EQ(3, node.past_detections_.size());
 }
 
 int main(int argc, char **argv)
