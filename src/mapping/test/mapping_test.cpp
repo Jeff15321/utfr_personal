e5f8fb901f0fef186bc94c0511777001715f7d59
Mark <markp566@hotmail.com>
2024-01-18 19:12:09 -0500

    changes to KNN tests

--- src/mapping/test/mapping_test.cpp
+++ src/mapping/test/mapping_test.cpp
@@ -34,34 +34,37 @@ TEST(BuildGraphNodeTest, kNNTest1)
     node.current_state_ = ego;
 
     // Custom failure message
-    ASSERT_EQ(0, node.current_state_.pose.pose.position.x); //<< "Unexpected value for position.x";
+    ASSERT_EQ(0, node.current_state_.pose.pose.position.x); << "Unexpected value for position.x";
 
     // Additional logging
-    //std::cout << "Debug info: " << node.current_state_.pose.pose.position.x << std::endl;
+    std::cout << "Debug info: " << node.current_state_.pose.pose.position.x << std::endl;
 
     // First round of cone detections
-    // utfr_msgs::msg::ConeDetections cones;
+    utfr_msgs::msg::ConeDetections cones;
 
-    // utfr_msgs::msg::Cone blue_cone;
-    // blue_cone.pos.x = 1;
-    // blue_cone.pos.y = 1;
-    // blue_cone.pos.z = 0;
-    // blue_cone.type = 1;
+    utfr_msgs::msg::Cone blue_cone;
+    blue_cone.pos.x = 1;
+    blue_cone.pos.y = 1;
+    blue_cone.pos.z = 0;
+    blue_cone.type = 1;
+
+    cones.left_cones.push_back(blue_cone);
+
+    //KNN once
+    node.KNN(cones);
+
+    // check if tree created
+    ASSERT_EQ(false, globalKDTreePtr == nullptr);
+
+    // check is added to past_detections_
+    ASSERT_EQ(1, past_detections_.size());
 
     //utfr_msgs::msg::Cone yellow_cone;
     //yellow_cone.pos.x = -1;
     //yellow_cone.pos.y = -1;
     //yellow_cone.pos.z = 0;
     //yellow_cone.type = 2;
-
-    // cones.left_cones.push_back(blue_cone);
     //cones.right_cones.push_back(yellow_cone);
-
-    //KNN once
-    // node.KNN(cones);
-
-    // Assert after a run
-    // ASSERT_EQ(1, node.past_detections_.size());
 
     // Assert after three more runs
     // ASSERT_EQ(false, globalKDTreePtr == nullptr);
