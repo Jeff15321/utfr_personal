a83f8a4fa8d99110e6dd68b56fbf09e571ca3b87
DanielAsadi <daniel.asadi91@gmail.com>
2024-01-28 14:40:49 -0500

    fix up some laucnfiles, add scripts

---
+++ scripts/p_sensors_bag.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Record cameras + LIDAR data
+# Usage: bash scripts/p_sensors_bag.sh <bag_name>
+
+cleanup() {
+    echo "Killing all ros2 processes"
+    kill $PID_RECORD
+    sleep 1
+    kill $PID_LAUNCH
+    exit
+}
+
+trap cleanup SIGINT
+
+cd ~/dv24
+source install/setup.bash
+ros2 launch launcher p_sensors.launch.py &
+PID_LAUNCH=$!
+
+sleep 30
+
+cd # TODO: ssd location
+ros2 bag record -s mcap /ouster/points /right_camera_node/images /left_camera_node/images -o "$1" &
+PID_RECORD=$!
+
+wait
---
+++ src/launcher/launch/p_sensors.launch.py
@@ -0,0 +1,30 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription, launch_description_sources
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Drivers
+    cam_dir = get_package_share_directory("arena_camera_node")
+    cam_launch = IncludeLaunchDescription(
+        launch_description_sources.PythonLaunchDescriptionSource(
+            cam_dir + "/launch/lucid_camera.launch.py"
+        )
+    )
+
+    lidar_dir = get_package_share_directory("ouster_ros")
+    lidar_launch = IncludeLaunchDescription(
+        launch_description_sources.PythonLaunchDescriptionSource(
+            lidar_dir + "/launch/driver.launch.py"
+        )
+    )
+
+    ld.add_action(cam_launch)
+    ld.add_action(lidar_launch)
+
+    return ld
