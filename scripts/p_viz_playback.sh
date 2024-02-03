55150a87fb54512c90174227d5f00931e5d84a1b
utfr-computer <team@fsaeutoronto.ca>
2024-02-02 19:28:02 -0500

    more fixes

--- scripts/p_viz_playback.sh
+++ scripts/p_viz_playback.sh
@@ -6,7 +6,7 @@
 
 cleanup() {
     echo "Killing all ros2 processes"
-    # kill $PID_RECORD
+    kill $PID_PLAYBACK
     kill $PID_BRIDGE
     kill $PID_VISUALIZATION
     kill $PID_LAUNCH
@@ -17,17 +17,17 @@ trap cleanup SIGINT
 
 cd ~/dv24
 source install/setup.bash
-ros2 launch launcher perception.launch.py &
+ros2 launch launcher p_stack.launch.py &
 PID_LAUNCH=$!
 ros2 launch visualization visualization.launch.py &
 PID_VISUALIZATION=$!
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
 PID_BRIDGE=$!
 
-# sleep 30
+sleep 30
 
-# cd "/media/utfr-dv/1tb ssd/rosbags"
-# ros2 bag record -s mcap /ouster/points /right_camera_node/images /left_camera_node/images -o "$1" &
-# PID_RECORD=$!
+cd "/media/utfr-dv/1tb ssd/rosbags"
+ros2 bag play -l "$1" &
+PID_PLAYBACK=$!
 
 wait
