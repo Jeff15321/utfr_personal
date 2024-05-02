07f7ff84afc516654680b2b3a7f76cb7b9fdc6e2
Yulu Jiang <89326638+yu1uuu@users.noreply.github.com>
2024-05-02 01:12:36 +0000

    fixed all build warnings

--- src/mapping/test/mapping_test.cpp
+++ src/mapping/test/mapping_test.cpp
@@ -142,52 +142,52 @@ using namespace utfr_dv::ekf;
 //   // ASSERT_EQ(3, node.past_detections_.size());
 // }
 
-// TEST(BuildGraphNodeTest, loopClosureTest1) {
-//   // Setup
-//   utfr_dv::build_graph::BuildGraphNode node;
-//   ASSERT_EQ(false, node.loop_closed_);
+TEST(BuildGraphNodeTest, loopClosureTest1) {
+  // Setup
+  utfr_dv::build_graph::BuildGraphNode node;
+  ASSERT_EQ(false, node.loop_closed_);
 
-//   // First round of cone detections
-//   utfr_msgs::msg::Cone blue_cone;
-//   blue_cone.type = 1;
-//   utfr_msgs::msg::Cone orange_cone;
-//   orange_cone.type = 4;
-//   node.past_detections_.push_back(std::make_pair(1, blue_cone));
-//   node.past_detections_.push_back(std::make_pair(2, orange_cone));
-//   std::vector<int> detected_cone_ids;
-//   detected_cone_ids.push_back(1);
-//   detected_cone_ids.push_back(2);
+  // First round of cone detections
+  utfr_msgs::msg::Cone blue_cone;
+  blue_cone.type = 1;
+  utfr_msgs::msg::Cone orange_cone;
+  orange_cone.type = 4;
+  node.past_detections_.push_back(std::make_pair(1, blue_cone));
+  node.past_detections_.push_back(std::make_pair(2, orange_cone));
+  std::vector<int> detected_cone_ids;
+  detected_cone_ids.push_back(1);
+  detected_cone_ids.push_back(2);
 
-//   // Run loop closure
-//   node.loopClosure(detected_cone_ids);
-//   ASSERT_EQ(false, node.loop_closed_);
-//   ASSERT_EQ(2, node.landmarkedID_);
-//   ASSERT_EQ(true, node.landmarked_);
+  // Run loop closure
+  node.loopClosure(detected_cone_ids);
+  ASSERT_EQ(false, node.loop_closed_);
+  ASSERT_EQ(2, node.landmarkedID_);
+  ASSERT_EQ(true, node.landmarked_);
 
-//   // Run loop closure again with the same cones
-//   node.loopClosure(detected_cone_ids);
-//   ASSERT_EQ(false, node.loop_closed_);
-//   ASSERT_EQ(2, node.landmarkedID_);
-//   ASSERT_EQ(true, node.landmarked_);
-//   ASSERT_EQ(false, node.out_of_frame_);
+  // Run loop closure again with the same cones
+  node.loopClosure(detected_cone_ids);
+  ASSERT_EQ(false, node.loop_closed_);
+  ASSERT_EQ(2, node.landmarkedID_);
+  ASSERT_EQ(true, node.landmarked_);
+  ASSERT_EQ(false, node.out_of_frame_);
 
-//   // Orange cone is now gone from view
-//   detected_cone_ids.erase(
-//       std::remove(detected_cone_ids.begin(), detected_cone_ids.end(), 2),
-//       detected_cone_ids.end());
-//   node.loopClosure(detected_cone_ids);
-//   ASSERT_EQ(false, node.loop_closed_);
-//   ASSERT_EQ(2, node.landmarkedID_);
-//   ASSERT_EQ(true, node.landmarked_);
-//   ASSERT_EQ(true, node.out_of_frame_);
+  // Orange cone is now gone from view
+  detected_cone_ids.erase(
+      std::remove(detected_cone_ids.begin(), detected_cone_ids.end(), 2),
+      detected_cone_ids.end());
+  node.loopClosure(detected_cone_ids);
+  ASSERT_EQ(false, node.loop_closed_);
+  ASSERT_EQ(2, node.landmarkedID_);
+  ASSERT_EQ(true, node.landmarked_);
+  ASSERT_EQ(true, node.out_of_frame_);
 
-//   // Orange cone is back in view, close loop
-//   detected_cone_ids.push_back(2);
-//   node.loopClosure(detected_cone_ids);
-//   ASSERT_EQ(false, node.loop_closed_);
-//   ASSERT_EQ(2, node.landmarkedID_);
-//   ASSERT_EQ(true, node.landmarked_);
-// }
+  // Orange cone is back in view, close loop
+  detected_cone_ids.push_back(2);
+  node.loopClosure(detected_cone_ids);
+  ASSERT_EQ(false, node.loop_closed_);
+  ASSERT_EQ(2, node.landmarkedID_);
+  ASSERT_EQ(true, node.landmarked_);
+}
 // TEST(EkfNodeTest, converlatlonalt2) {
 //   // utfr_dv::build_graph::BuildGraphNode node;
 //   utfr_dv::ekf::EkfNode node;
