e88fccac5a3270401842c0c32d36ed049bd2794d
Yulu Jiang <89326638+yu1uuu@users.noreply.github.com>
2024-05-02 03:29:32 +0000

    took out unecessary lines

--- src/mapping/test/mapping_test.cpp
+++ src/mapping/test/mapping_test.cpp
@@ -21,14 +21,6 @@ using namespace utfr_dv::build_graph;
 using namespace utfr_dv::compute_graph;
 using namespace utfr_dv::ekf;
 
-// initialize ROS
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    ::testing::InitGoogleTest(&argc, argv);
-    int result = RUN_ALL_TESTS();
-    rclcpp::shutdown();
-    return result;
-}
 
 TEST(KNNSearchNodeTest, KNNConeDetections) {
   // Setup
@@ -82,6 +74,13 @@ TEST(KNNSearchNodeTest, KNNConeDetections) {
   ASSERT_EQ(0, node.potential_cones_.size());
 
   cones = empty;
+}
+
+// initialize ROS
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
 // TEST(KNNSearchNodeTest, kNNTest1) {
