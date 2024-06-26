9b2b60a413c77b0906c64a073f33924e03d9c549
alexwonseokcho <wonseok4840@gmail.com>
2024-06-26 01:56:46 -0400

    added a parameter to enable lidar only detection

--- src/perception/launch/extrinsics.launch.py
+++ src/perception/launch/extrinsics.launch.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 import os.path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -10,6 +11,42 @@ def generate_launch_description():
 
     # TODO - figure out how to read from extrinsics.json or just put it in
     # config.yaml
+
+    config_path = os.path.join(
+        get_package_share_directory("perception"), "config", "config.yaml"
+    )
+
+    with open(config_path, "r") as file:
+        config_param = yaml.safe_load(file)["perception_node"]
+    lidar_only_detection = config_param["ros__parameters"]["lidar_only_detection"]
+
+    lidar = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=[
+            "--x",
+            "0",
+            "--y",
+            "0",
+            "--z",
+            "1.5",
+            "--yaw",
+            "3.141592",
+            "--pitch",
+            # "0.122173",
+            "0.0",
+            "--roll",
+            "0.0",
+            "--frame-id",
+            "baselink",
+            "--child-frame-id",
+            "lidar",
+        ],
+    )
+
+    ld.add_action(lidar)
+    if lidar_only_detection:
+        return ld
 
     left_camera = Node(
         package="tf2_ros",
@@ -69,31 +106,7 @@ def generate_launch_description():
         ],
     )
 
-    lidar = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=[
-            "--x",
-            "0",
-            "--y",
-            "0",
-            "--z",
-            "1.5",
-            "--yaw",
-            "3.141592",
-            "--pitch",
-            # "0.122173",
-            "0.0",
-            "--roll",
-            "0.0",
-            "--frame-id",
-            "baselink",
-            "--child-frame-id",
-            "lidar",
-        ],
-    )
-
     ld.add_action(left_camera)
     ld.add_action(right_camera)
-    ld.add_action(lidar)
+
     return ld
